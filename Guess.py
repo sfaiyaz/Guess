0ff49556eeccb220ef739a244992ad215b96115a
sfaiyaz <sajidafaiyaz@yahoo.ca>
2024-04-10 23:27:05 -0500

    Final Guess Game

--- Guess.py
+++ Guess.py
@@ -1,14 +1,14 @@
 import random
 
-def guess_number():
+def guess_number(max_attempts):
     # Generate a random number between 1 and 100
     secret_number = random.randint(1, 100)
-    attempts = 0
 
     print("Welcome to Guess the Number game!")
     print("I'm thinking of a number between 1 and 100. Can you guess it?")
 
-    while True:
+    attempts = 0
+    while attempts < max_attempts:
         guess = int(input("Enter your guess: "))
         attempts += 1
         
@@ -19,14 +19,21 @@ def guess_number():
         else:
             print(f"Congratulations! You've guessed the number {secret_number} correctly!")
             print(f"It took you {attempts} attempts.")
-            break
+            return True
 
-    play_again = input("Do you want to play again? (yes/no): ")
-    return play_again.lower() == "yes"
+    print(f"Sorry, you've used all {max_attempts} attempts. The correct number was {secret_number}.")
+    return False
 
 if __name__ == "__main__":
     while True:
-        if not guess_number():
-            print("Thanks for playing!")
-            break
-
+        max_attempts = int(input("Enter the number of attempts you want to have: "))
+        if guess_number(max_attempts):
+            play_again = input("Do you want to play again? (yes/no): ")
+            if play_again.lower() != "yes":
+                print("Thanks for playing!")
+                break
+        else:
+            play_again = input("Do you want to try again with a different number of attempts? (yes/no): ")
+            if play_again.lower() != "yes":
+                print("Thanks for playing!")
+                break
